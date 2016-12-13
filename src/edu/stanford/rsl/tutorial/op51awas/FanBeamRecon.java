f8e9cc057e9dfcda08168ed96ac237a10ee94ef8
Eric Goppert <op51awas@stud.informatik.uni-erlangen.de>
2016-12-13 11:49:05 +0100

    fuilure in detector position

--- src/edu/stanford/rsl/tutorial/op51awas/FanBeamRecon.java
+++ src/edu/stanford/rsl/tutorial/op51awas/FanBeamRecon.java
@@ -1,6 +1,11 @@
 package edu.stanford.rsl.tutorial.op51awas;
 
+import ij.ImageJ;
+
+import java.util.ArrayList;
+
 import edu.stanford.rsl.conrad.data.numeric.Grid2D;
+import edu.stanford.rsl.conrad.data.numeric.InterpolationOperators;
 import edu.stanford.rsl.conrad.geometry.shapes.simple.Box;
 import edu.stanford.rsl.conrad.geometry.shapes.simple.PointND;
 import edu.stanford.rsl.conrad.geometry.shapes.simple.StraightLine;
@@ -18,14 +23,22 @@ public class FanBeamRecon {
 	private double sourceDetDist;
 
 	public static void main(String[] args) {
-
+		
+		SimplePhantom phant = new SimplePhantom(250, 250, new double[]{0.5, 0.5});
+		FanBeamRecon rec = new FanBeamRecon(250, 250, 1.0, 1200, 750);
+		
+		Grid2D sino = rec.computeFanogram(phant, 1.0);
+		
+		new ImageJ();
+		phant.show();
+		sino.show();
 	}
 
-	public FanBeamRecon(int numOfProjections, int detectorSize, double detectorSpacing, double dSD) {
+	public FanBeamRecon(int numOfProjections, int detectorSize, double detectorSpacing, double dSD, double dSI) {
 		this.detectorSize = detectorSize;
 		this.detectorSpacing = detectorSpacing;
 		this.numOfProjections = numOfProjections;
-		this.sourceIsoDist = dSD/2;
+		this.sourceIsoDist = dSI;
 		this.sourceDetDist = dSD;
 
 		double fanAngle = Math.atan(((double)detectorSize)/(2 * dSD));
@@ -58,24 +71,73 @@ public class FanBeamRecon {
 			curAngle = i*angularIncrement;
 			
 			// determine source position in world coordinates
-			double x = Math.cos(curAngle) * sourceIsoDist;
-			double y = Math.sin(curAngle) * sourceIsoDist;
-			PointND sourcePos = new PointND(x, y);
+			double xDir = Math.cos(curAngle);
+			double yDir = Math.sin(curAngle);
+			PointND sourcePos = new PointND(sourceIsoDist*xDir, sourceIsoDist*yDir, 0);
 			
 			// determine detector middle, thanks to symmetry just the negative of source position
-			PointND detectorMiddle = new PointND(-x, -y);
+			PointND detectorMiddle = new PointND((-sourceDetDist+sourceIsoDist)*xDir, (-sourceDetDist+sourceIsoDist)*yDir, 0);
+			
+			if (curAngle > Math.PI/2-angularIncrement && curAngle < Math.PI/2+angularIncrement) {
+				System.out.println(sourcePos);
+			}
+			SimpleVector detectorMid = detectorMiddle.getAbstractVector().clone();
 			
 			// determine normal vector of conjunction between source position and detector middle
-			double normDirX = Math.cos(Math.PI - curAngle);
-			double normDirY = Math.sin(Math.PI - curAngle);
-			SimpleVector dir = new SimpleVector(normDirX, normDirY);
-						
+//			double normDirX = Math.cos(Math.PI - curAngle);
+//			double normDirY = Math.sin(Math.PI - curAngle);
+			double normDirX = xDir;
+			double normDirY = -yDir;
+			SimpleVector dir = new SimpleVector(normDirX, normDirY, 0);
+			
+			double detectorHalf = (detectorSize*detectorSpacing)/2;
 			for (int s = 0; s < detectorSize; s++) {
+				double step = 0.0;
+				if (s < detectorSize/2) {
+					step = -detectorHalf + s * detectorSpacing;
+				} else {
+					step = s * detectorSpacing - detectorHalf;
+				}
 				
+				SimpleVector curStep = dir.multipliedBy(step).clone();
+				curStep.add(detectorMid);
+				if (s == 0) {
+					System.out.println(curStep.toString());
+				}
+				PointND curPoint = new PointND(curStep);
+				StraightLine line = new StraightLine(sourcePos, curPoint);
+				ArrayList<PointND> hits = box.intersect(line);
+				
+				if (hits.size() == 2) {
+					PointND start = hits.get(0);
+					PointND end = hits.get(1);
+					SimpleVector lineDir = end.getAbstractVector().clone();
+					dir.subtract(start.getAbstractVector());
+					double length = lineDir.normL2();
+					
+					dir.divideBy(length / samplingRate);
+					
+					float sum = 0.0f;
+					for (int t = 0; t < (length/samplingRate); t++) {
+						PointND cur = new PointND(start);
+						cur.getAbstractVector().add(dir.multipliedBy(t));
+						double curX = cur.getCoordinates()[0] * image.getSpacing()[0];
+						double curY = cur.getCoordinates()[1] * image.getSpacing()[1];
+						
+						double[] coords = image.physicalToIndex(curX, curY);
+						
+						sum += InterpolationOperators.interpolateLinear(image, coords[0], coords[1]);
+						
+					}
+					
+					sum /= samplingRate;
+					sinogram.setAtIndex(s, i, sum);
+					
+				}
 			}
 		}
 
-		return null;
+		return sinogram;
 	}
 
 	public void setAngularIncrement() {
